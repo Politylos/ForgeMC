7a8480958669c2ccc26598bcc624b3eb95182ffb
Politylos <politylosophia@gmail.com>
2024-04-10 20:10:39 +1000

    init

---
+++ GetForgeServer.sh
@@ -0,0 +1,3 @@
+curl -s -o ForgeInstall.jar https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.2.0/forge-1.20.1-47.2.0-installer.jar
+java -jar ForgeInstall.jar --installServer
+rm ForgeInstall.jar
